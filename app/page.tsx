12f3c13951d187aa923407e3ae77ae969cc1a2a2
Roger Fan <rogerjoeyfan@gmail.com>
2024-03-13 17:18:29 -0700

    initial changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,112 +1,95 @@
-import Image from "next/image";
+import Image from 'next/image'
+
+
+const InfoBlock = (props: { header: string, children:string }) => {
+  return (
+    <div>
+      <div className="text-3xl font-bold">{props.header}</div>
+      <div>{props.children}</div>
+    </div>
+  )
+}
+
+const ScheduleItem = (props: { start: string, end:string, children:string}) => {
+  return (
+    <div className="md:text-lg"><span className="font-bold">{">"}{props.start}-{props.end}:</span> {props.children}</div>
+  )
+}
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main>
+      <div className="px-10 pb-16 pt-32 md:px-20">
+        <h1 className="font-bold text-4xl md:text-7xl">GUNN MATH COMPETITION</h1>
+        <h2 className="text-md md:text-xl">March 24th, 2024  ///  8:30 AM - 4:30 PM  ///  Gunn High School</h2>
+        
+        <div className="absolute right-5 top-[20%] z-[-1] invisible md:visible"><Image src="/fsh.png" width={500} height={400} alt="GMC logo" /></div>
+
+        <div className="mt-10 flex gap-10">
+          <div className="flex flex-col gap-5"> 
+            <InfoBlock header="2 Divisions">Beginner & Advanced (AIME Qualifiers)</InfoBlock>
+            <InfoBlock header="Free Lunch">For all competitors</InfoBlock>
+          </div>
+          <div className="flex flex-col gap-5">
+            <InfoBlock header="3 Rounds">Individual, Guts, & Team</InfoBlock>
+            <InfoBlock header="Over $1000 in Prizes">For the top teams and individuals</InfoBlock>
+          </div>
         </div>
+        <a target="_blank" rel="noopener noreferrer" href="https://docs.google.com/forms/d/e/1FAIpQLSenCWtv3C2JBd_ibmI-xp7mkOTdQYrtLHhC8BSwOlm_pW3dvg/viewform">
+          <div className="inline-block font-bold text-3xl mt-12 py-5 px-16 rounded-lg bg-[#DEC880] hover:rotate-6 transition-all">Register</div>
+        </a>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="px-10 py-10 md:px-20">
+        <h1 className="font-bold text-3xl md:text-5xl">About</h1>
+        <p className="max-w-xl text-md md:text-lg my-3">
+          The third annual Gunn Math Competition is hosted by dedicated students from Gunn Math Circle and will be held at Gunn High School on Sunday, March 24th 2024.
+        </p>
+
+        <h2 className="font-bold text-3xl mt-10">Divisions</h2>
+        <p className="max-w-xl text-md md:text-lg my-3">
+          The competition has 2 divisions, A and B. Division B will be for those who are relatively new to competition math and are generally taking a math class before calculus. Division A will be for those who are familiar with competition math, and will thus have harder problems. Participants who have participated in the AIME must compete in Division A.
+        </p>
+
+        <h2 className="font-bold text-3xl mt-10">Teams</h2>
+        <p className="max-w-xl text-md md:text-lg my-3">
+          Students will compete in teams of 4. (Teams of 2 or 3 are okay as well, but they will be at a disadvantage during the team rounds as they have fewer people.) Students may compete individually, but we strongly encourage competing in teams to get the most out of the competition.
+        </p>
+
+        <h2 className="font-bold text-3xl mt-10">Test Format</h2>
+        <p className="max-w-xl text-md md:text-lg my-3">
+          Individual Round: 60 minutes, 10 problems<br/>
+          Team Round: 60 minutes, 15 problems<br />
+          Guts Round: 75 minutes, 3x8 problems<br />
+          Tiebreaker Round: 15 minutes, 3 problems (Qualifiers Only)
+        </p>
+
+        <p className="max-w-xl text-md md:text-lg my-3">
+          In the Guts Round, problems are released in sets of 3. Students must submit answers to the problems in each set before moving on to the next set. This year, we will have multiple activities in the afternoon for students to choose from, including an Estimathon. More details about the exact activities will be announced soon!
+        </p>
+
+        <h2 className="font-bold text-3xl mt-10">Difficulty</h2>
+        <p className="max-w-xl text-md md:text-lg my-3">
+          For Div. A, the difficulty of the problems will range from early AMC 10 to mid or late AIME. For Div. B, the difficulty of the problems will range from AMC 8 to late AMC 10. In general, the Guts and Team rounds will be more difficult than the Individual round, as participants are working as a team.
+        </p>
       </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      <div className="flex flex-col items-center py-32">
+        <h1 className="font-bold text-3xl md:text-5xl my-5">Schedule</h1>
+        <ScheduleItem start="8:30" end="9:15">Registration</ScheduleItem>
+        <ScheduleItem start="9:30" end="10:40">Individual Round</ScheduleItem>
+        <ScheduleItem start="10:40" end="12:00">Team Round</ScheduleItem>
+        <ScheduleItem start="12:10" end="13:00">Lunch</ScheduleItem>
+        <ScheduleItem start="13:00" end="14:20">Guts Round</ScheduleItem>
+        <ScheduleItem start="14:30" end="15:45">Activities / Tiebreaks</ScheduleItem>
+        <ScheduleItem start="16:00" end="16:30">Awards Ceremony</ScheduleItem>
+      </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="flex flex-col items-center py-32 gap-5">
+        <h1 className="font-bold text-3xl md:text-5xl my-5">Sponsors</h1>
+        <a target="_blank" rel="noopener noreferrer" href="https://www.janestreet.com/" ><Image alt="Jane Street" width={300} height={50} src="/janestreet.png"/></a>
+        <a target="_blank" rel="noopener noreferrer" href="https://wolfram.com/" ><Image alt="Wolfram" width={300} height={50} src="/wolfram.png" /></a>
+        <a target="_blank" rel="noopener noreferrer" href="https://live.poshenloh.com/" ><Image alt="Live" width={200} height={50} src="/live.png" /></a>
       </div>
     </main>
   );
