3efb8c9fbfad7d47c2eac0efc6a6f5b69dd4cc23
maryofmerry <yumary6279@gmail.com>
2025-03-03 16:40:18 -0800

    Add AOPS as sponsor

--- app/page.tsx
+++ app/page.tsx
@@ -123,10 +123,10 @@
           <Sponsor src="/janestreetblack.png" url="https://www.janestreet.com/" alt="Jane Street" />
           <Sponsor src="/HRT.png" url="https://www.hudsonrivertrading.com/" alt="Hudson River Trading" />
           <Sponsor src="/dailychallenge.png" url="https://daily.poshenloh.com/" alt="Po Shen Loh Live" />
+          <Sponsor src="/aops.png" url="https://artofproblemsolving.com/" alt="Art of Problem Solving" />
           {/*
           
           <Sponsor src="/thinkfund.png" url="https://www.mitchellparkteenservices.org/thinkfund" alt="Thinkfund" square />
-          <Sponsor src="/aops.png" url="https://artofproblemsolving.com/" alt="Art of Problem Solving" />
           <Sponsor src="/wolfram.png" url="https://www.wolfram.com/" alt="Wolfram" />
           
         
