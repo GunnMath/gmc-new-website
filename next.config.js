41c980d8c0816b48449f0068efee5e52a5d0f48c
maryofmerry <yumary6279@gmail.com>
2025-03-17 22:23:27 -0700

    Fixes

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix: process.env.NODE_ENV === "production" ? "/gmc-new-website/" : "",
+};
+
+module.exports = nextConfig;
