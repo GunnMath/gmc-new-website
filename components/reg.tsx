6b87f15c8a7087fac53e868bca3df0e57f8be1f5
maryofmerry <yumary6279@gmail.com>
2025-03-17 22:28:59 -0700

    Delete files in gh-pages branch

--- components/reg.tsx
+++
@@ -1,9 +0,0 @@
-export default function RegisterButton () {
-    return (
-        <a target="_blank" rel="noopener noreferrer" href="https://docs.google.com/forms/d/e/1FAIpQLSek6UnuM0p-Qx9Nntd_Cva_dOnVLDZq2WwoqYzN2lyjPkXM2g/viewform">
-              <div className="inline-block font-bold text-3xl py-5 px-16 rounded-lg bg-[#DEC880] hover:rotate-6 transition-all">
-                Registration
-              </div>
-            </a>
-    )
-}
--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: "export",
-    basePath: "", 
-    assetPrefix: "",
-  };
-  
-  export default nextConfig;
-  
